97dd61e39a390be5c239458996446d58e7c2161f
1jsingh <1.jaskiratsingh@gmail.com>
2019-11-16 12:48:56 +0000

    feat: better model with per face key points

--- models.py
+++ models.py
@@ -10,7 +10,8 @@ import torch.nn.init as I
 
 class Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self,num_layers=3,num_filters=16,kernel_size=(3,3),drop_prob1=0.2, 
+                 drop_prob2 = 0.5,pool_layers=[0,1],drop_layers=[0,1]):
         super(Net, self).__init__()
         
         ## TODO: Define all the layers of this CNN, the only requirements are:
@@ -22,18 +23,46 @@ class Net(nn.Module):
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
 
         # input: (1,224,224)
-        self.conv1 = nn.Conv2d(1, 32, 3,padding =) # (32,224,224)
+        in_channels = 1
+        out_channels = num_filters
+        dim = 224
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        layers= []
+        for i in range(num_layers):
+            layers.append(nn.Conv2d(in_channels,out_channels,kernel_size=kernel_size,padding=1))
+            layers.append(nn.ReLU())
+            
+            if i in drop_layers:
+                layers.append(nn.Dropout2d(drop_prob1))
+                
+            layers.append(nn.BatchNorm2d(out_channels))
+            
+            in_channels = out_channels
+            out_channels *= 2
+            dim = (dim+2-kernel_size[0])+1
+            
+            if i in pool_layers:
+                layers.append(nn.MaxPool2d(2,2))
+                dim = dim//2
+            
         
+        # feature block
+        self.features = nn.Sequential(*layers)
+        
+        # fc layers
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.dropout1 = nn.Dropout(drop_prob2)
+        self.fc1 = nn.Linear(in_channels,256)
+        self.fc2 = nn.Linear(256,136)
 
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        net = self.features(x)
+        net = self.avgpool(net)
         
+        net = net.view(net.size(0),-1)
         
-        # a modified x, having gone through all the layers of your model, should be returned
-        return x
+        net = self.dropout1(net)
+        net = F.relu(self.fc1(net))
+        net = self.fc2(net)
+        return net
